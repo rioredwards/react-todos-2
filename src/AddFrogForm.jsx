f499d72e26defad8e409c6bbf7d6163057bd62fd
Rio Edwards <rioredwards@gmail.com>
2024-06-25 13:40:27 -0700

    create a form for submitting new frogs

---
+++ src/AddFrogForm.jsx
@@ -0,0 +1,9 @@
+export default function AddFrogForm() {
+  return (
+    <form>
+      <input id="frogTitle" />
+      <label htmlFor="frogTitle">Enter frog type</label>
+      <button type="submit">Add Frog</button>
+    </form>
+  );
+}
--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import "./App.css";
 import FrogsList from "./FrogsList.jsx";
+import AddFrogForm from "./AddFrogForm.jsx";
 
 function App() {
   return (
     <>
       <h1>Frogs</h1>
+      <AddFrogForm />
       <FrogsList />
     </>
   );

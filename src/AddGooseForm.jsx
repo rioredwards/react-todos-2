2dc66ccb1605ed19c327cd04325827536fc78605
Rio Edwards <rioredwards@gmail.com>
2024-07-02 13:06:04 -0700

    Add new GooseListItem component and handler function for submitting AddGooseForm

--- src/AddGooseForm.jsx
+++ src/AddGooseForm.jsx
@@ -1,8 +1,16 @@
 export default function AddGooseForm() {
+  function handleAddGoose(event) {
+    event.preventDefault();
+    const form = event.target;
+    const input = form.querySelector("input");
+    const inputValue = input.value;
+    console.log(inputValue);
+  }
+
   return (
-    <form>
-      <label htmlFor="gooseTitle">Enter goose type</label>
-      <input id="gooseTitle" />
+    <form onSubmit={handleAddGoose}>
+      <label htmlFor="gooseType">Enter goose type</label>
+      <input name="type" id="gooseType" />
       <button type="submit">Add Goose</button>
     </form>
   );

716b1602323c48757dd93ab55153126bf48f8bd5
Rio Edwards <rioredwards@gmail.com>
2024-07-30 13:40:02 -0700

    Add Auto-Focus to Input

--- src/InputWithLabel.jsx
+++ src/InputWithLabel.jsx
@@ -1,4 +1,16 @@
+import { useEffect, useRef } from "react";
+
 export function InputWithLabel(props) {
+  // useRef can be used to store DOM elements in state
+  // Unlike variables from useState, ref's don't cause re-renders
+  const inputRef = useRef();
+
+  console.log(inputRef.current);
+
+  useEffect(() => {
+    inputRef.current.focus();
+  });
+
   return (
     <>
       <label htmlFor="gooseType">{props.children}</label>
@@ -7,6 +19,7 @@ export function InputWithLabel(props) {
         onChange={props.handleTitleChange}
         name="type"
         id="gooseType"
+        ref={inputRef}
       />
     </>
   );

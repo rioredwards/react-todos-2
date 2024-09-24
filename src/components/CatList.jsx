ad4ab8b75c26c781e0d901a868ae59a1636edaa9
Rio Edwards <rioredwards@gmail.com>
2024-09-24 11:25:21 -0700

    Organize components into components dir

--- src/components/CatList.jsx
+++ src/components/CatList.jsx
@@ -5,7 +5,12 @@ export default function CatsList({ catList, onRemoveCat }) {
   return (
     <ul>
       {catList.map((cat) => (
-        <CatListItem key={cat.id} title={cat.title} onRemoveCat={onRemoveCat} id={cat.id} />
+        <CatListItem
+          key={cat.id}
+          title={cat.title}
+          onRemoveCat={onRemoveCat}
+          id={cat.id}
+        />
       ))}
     </ul>
   );
